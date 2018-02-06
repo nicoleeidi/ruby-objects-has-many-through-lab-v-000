abd0faa972f41c7423e10fea76ad9cbd743bcac9
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-06 17:31:07 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,7 +1,13 @@
-class Doctor 
+class Doctor
   attr_accessor :name
   def initialize(name)
     @name=name
     @appointments= []
-  
+  end 
+  def appointments
+    @appointments
+  end
+  def add_appointment(appointment)
+    @appointments << appointment 
+
 end 
