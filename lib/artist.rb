2f6d7ac8752c58e417b7de62d69a7c9c7d6dd0c1
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-06 17:18:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,13 +3,13 @@ class Artist
   def initialize(name)
     @name=name
     @songs=[]
-  end 
+  end
   def songs
     @songs
-  end 
+  end
   def add_song(song)
     @songs << song
-    song.artist= self 
-  end 
-end 
+    song.artist= self
+  end
+end
     
